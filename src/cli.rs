54afa4d7a4e6b0b3283eebb6e7405cfb5b08cb46
pohe <ksiwt.jp@gmail.com>
2022-11-21 22:16:05 +0900

    rename author.

--- src/cli.rs
+++ src/cli.rs
@@ -4,7 +4,7 @@ pub fn parse() -> clap::ArgMatches {
     Command::new("Hurust")
         .about("Command line tool which act as wrapper of hugo new command")
         .version("1.0.0")
-        .author("mudrk")
+        .author("pohe")
         .subcommand_required(true)
         // Query subcommand
         .subcommand(
